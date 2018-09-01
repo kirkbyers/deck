c7c39a0bf571cdf8a614e7a4df5d926615cd8bab
kirkbyers <kirklbyers@gmail.com>
2018-09-01 16:58:25 -0500

    Complete tests

--- card.go
+++ card.go
@@ -4,6 +4,9 @@ package deck
 
 import (
 	"fmt"
+	"math/rand"
+	"sort"
+	"time"
 )
 
 // Suit is the "family" of a card
@@ -61,12 +64,88 @@ func (c Card) String() string {
 }
 
 // New creates a new deck of Cards
-func New() []Card {
+func New(opts ...func([]Card) []Card) []Card {
 	var cards []Card
 	for suit := minSuit; suit <= maxSuit; suit++ {
 		for rank := minRank; rank <= maxRank; rank++ {
 			cards = append(cards, Card{Rank: rank, Suit: suit})
 		}
 	}
+	for _, opt := range opts {
+		cards = opt(cards)
+	}
 	return cards
+}
+
+// DefaultSort is the the func that sorts the deck to its New state
+func DefaultSort(cards []Card) []Card {
+	sort.Slice(cards, Less(cards))
+	return cards
+}
+
+// Sort lets the user define and impl a custom way to sort the deck given a less function
+func Sort(less func(cards []Card) func(i, j int) bool) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		sort.Slice(cards, less(cards))
+		return cards
+	}
+}
+
+// Less is the default less function for DefaultSort
+func Less(cards []Card) func(i, j int) bool {
+	return func(i, j int) bool {
+		return absRank(cards[i]) < absRank(cards[j])
+	}
+}
+
+// absRank creates a value that lets you compare cards cross suits
+func absRank(c Card) int {
+	return int(c.Suit)*int(maxRank) + int(c.Rank)
+}
+
+// Shuffle randomly sorts cards
+func Shuffle(cards []Card) []Card {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	ret := make([]Card, len(cards))
+	for i, j := range r.Perm(len(cards)) {
+		ret[i] = cards[j]
+	}
+	return ret
+}
+
+// Jokers inserts n number of Jokers into deck
+func Jokers(n int) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		for i := 0; i < n; i++ {
+			cards = append(cards, Card{
+				Suit: Joker,
+				Rank: Rank(i),
+			})
+		}
+		return cards
+	}
+}
+
+// Filter takes a function that returns true if a card is to be filtered out
+func Filter(f func(card Card) bool) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		var ret []Card
+		for _, card := range cards {
+			if !f(card) {
+				ret = append(ret, card)
+			}
+		}
+		return ret
+	}
+}
+
+// Deck duplicates the deck n number of times
+func Deck(n int) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		var ret []Card
+		for i := 0; i < n; i++ {
+			ret = append(ret, cards...)
+		}
+		return ret
+	}
 }
--- card_test.go
+++ card_test.go
@@ -26,3 +26,73 @@ func TestNew(t *testing.T) {
 		t.Errorf("Wrong number of cards in a new deck. Got %d, expected %d", len(cards), expectedCardsLen)
 	}
 }
+
+func TestDefaultSort(t *testing.T) {
+	cards := New(DefaultSort)
+	expectedCard := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != expectedCard {
+		t.Errorf("Expected %s as first card. Received %s", expectedCard, cards[0])
+	}
+}
+func TestSort(t *testing.T) {
+	cards := New(Sort(Less))
+	expectedCard := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != expectedCard {
+		t.Errorf("Expected %s as first card. Received %s", expectedCard, cards[0])
+	}
+}
+
+func TestShuffle(t *testing.T) {
+	controlDeck := New(DefaultSort)
+	testDeck := New(DefaultSort, Shuffle)
+	for i, card := range testDeck {
+		controlCard := controlDeck[i]
+		if absRank(controlCard) != absRank(card) {
+			return
+		}
+	}
+	t.Error("Shuffled deck was in the same order as DefaultSort'd deck.")
+}
+
+func TestJokers(t *testing.T) {
+	expectedJokerCount := 4
+	cards := New(Jokers(expectedJokerCount))
+	count := 0
+	for _, c := range cards {
+		if c.Suit == Joker {
+			count++
+		}
+	}
+	if count != expectedJokerCount {
+		t.Errorf("Expected %d Jokers. Found %d.", expectedJokerCount, count)
+	}
+}
+
+func TestFilter(t *testing.T) {
+	filterCards := []Rank{Ace, Jack, Queen, King}
+	filterFn := func(card Card) bool {
+		for _, rank := range filterCards {
+			if card.Rank == rank {
+				return true
+			}
+		}
+		return false
+	}
+	cards := New(Filter(filterFn))
+	for _, card := range cards {
+		for _, rank := range filterCards {
+			if card.Rank == rank {
+				t.Errorf("Expected rank %s to be filtered from deck.", card.Rank.String())
+			}
+		}
+	}
+}
+
+func TestDeck(t *testing.T) {
+	expectedDeckCopies := 3
+	controlDeck := New()
+	actualDeck := New(Deck(expectedDeckCopies))
+	if len(actualDeck) != expectedDeckCopies*len(controlDeck) {
+		t.Errorf("Expected deck to be %d times larger than default. Got %d.", expectedDeckCopies, len(actualDeck)/len(controlDeck))
+	}
+}
